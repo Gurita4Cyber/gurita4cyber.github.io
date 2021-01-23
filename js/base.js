966560fe5599b5d1960a93c9685125b45d3e41e0
Gurita4Cyber <student.linuxs@gmailcom>
2021-01-23 20:30:25 +0700

    speed optimasi

--- js/base.js
+++ js/base.js
@@ -35,6 +35,30 @@ var back_to_top = function(){
   });	
 };
 
+var w3IncludeHTML = function(){
+  var z, i, a, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    if (z[i].getAttribute("w3-include-html")) {
+      a = z[i].cloneNode(false);
+      file = z[i].getAttribute("w3-include-html");
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+          a.removeAttribute("w3-include-html");
+          a.innerHTML = xhttp.responseText;
+
+          z[i].parentNode.replaceChild(a, z[i]);
+          w3IncludeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  } 
+}; 
+
 
 var choose_option_button = function(){
   "use strict";
@@ -81,6 +105,7 @@ $(document).ready(function(){
   choose_option_button();
   popup_box();
   reloadup();
+  w3IncludeHTML();
 });
 
 
